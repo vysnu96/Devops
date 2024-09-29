235305175520e66ce96fb5a2a8c62ac8877474b6
vysnu96 <81423989+vysnu96@users.noreply.github.com>
2024-09-29 19:03:58 +0530

    Create movie-updates.py

---
+++ movie-updates/movie-updates.py
@@ -0,0 +1,74 @@
+from bs4 import BeautifulSoup
+import requests
+
+# Getting the response from the torrent website
+try:
+    response = requests.get('https://www.1tamilmv.tf/')
+    response.raise_for_status()  # Raise an exception for HTTP errors
+    print("Website accessed successfully!")
+except requests.exceptions.HTTPError as http_err:
+    print(f"HTTP error occurred: {http_err}")
+except requests.exceptions.ConnectionError as conn_err:
+    print(f"Connection error occurred: {conn_err}")
+except requests.exceptions.Timeout as timeout_err:
+    print(f"Timeout error occurred: {timeout_err}")
+except requests.exceptions.RequestException as req_err:
+    print(f"An error occurred: {req_err}")
+
+# Beautify the response
+beauty = BeautifulSoup(response.text, 'html.parser')
+# List of styles to ignore
+ignore_styles = ["color:#0000ff;", "color:#000099;"]
+
+links = beauty.find_all('a')  # Find all the hyperlinks
+
+hyperlinkCounter = 0  # counter to keep track of no. of films or whatever
+
+# Getting the list of web series, TV Shows and others
+generic_list = []
+for i, link in enumerate(links):
+    if hyperlinkCounter >= 10:
+        break
+    if i > 34:
+        custom_color = link.find('span', style=lambda style: style in ignore_styles if style else False)
+        if custom_color:
+            continue
+
+        span = link.find('span')
+        if span:  # Ensure there's a <span> inside the <a>
+            hyperlinkCounter = hyperlinkCounter + 1
+            print(hyperlinkCounter, span.text)
+            generic_list.append(span.text)
+        else:  # If no <span> exists, extract the direct text inside <a>
+            hyperlinkCounter = hyperlinkCounter + 1
+            print(hyperlinkCounter, link.text, hyperlinkCounter)
+            generic_list.append(link.text)
+
+# Getting the list of movies in all languages
+movies_list = []
+p_tags = beauty.find_all('span', style='color:#000000;')
+for i, element in enumerate(p_tags):
+    if i > 10:
+        break
+    print(i, element.text)
+    movies_list.append(element.text)
+
+generic_text = ("List of latest 10 series, english movies listed in website. Our data may vary. Please check website "
+                "for integrity\n")
+movies_text = ("List of latest 10 movies in all languages listed in website. Our data may vary. Please "
+               "check website for integrity\n")
+
+generic = generic_text + "\n".join(generic_list)
+movies = movies_text + "\n".join(movies_list)
+
+URL = "https://api.telegram.org/bot/sendMessage"
+header = {"Content-Type": "application/json"}
+
+generic_payload = {'chat_id': , 'text': generic}
+generic_res = requests.post(URL, headers=header, json=generic_payload)
+print(generic_res.json())
+
+movies_payload = {'chat_id': , 'text': movies}
+movies_res = requests.post(URL, headers=header, json=movies_payload)
+print(movies_res.json())
+
