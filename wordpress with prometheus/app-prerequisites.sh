50dac9ab77d14008cfc793686c9526536821c5e7
vysnu96 <81423989+vysnu96@users.noreply.github.com>
2024-11-13 15:07:10 +0530

    Update app-prerequisites.sh

--- wordpress with prometheus/app-prerequisites.sh
+++ wordpress with prometheus/app-prerequisites.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 kubectl create configmap --from-file prometheus.yml prometheus-configuration
-kubectl create configmap --from-file alert-manager.yaml alert-configmap
+kubectl create configmap --from-file alert-manager.yaml alert-config
 kubectl create configmap --from-file rules.yaml rules-config
 kubectl create sa prometheus
 kubectl create token prometheus
