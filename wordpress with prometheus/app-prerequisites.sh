c42fbc0b6b32159510fb0b222c8c25fc44cc6444
vysnu96 <81423989+vysnu96@users.noreply.github.com>
2024-11-18 12:35:46 +0530

    Update app-prerequisites.sh

--- wordpress with prometheus/app-prerequisites.sh
+++ wordpress with prometheus/app-prerequisites.sh
@@ -5,3 +5,5 @@ kubectl create configmap --from-file alertmanager.yml alert-config
 kubectl create configmap --from-file rules.yaml rules-config
 kubectl create sa prometheus
 kubectl create token prometheus
+git clone https://github.com/kubernetes/kube-state-metrics.git /root/kube
+kubectl apply -f /root/kube/examples/standard
