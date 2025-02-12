91b1bfb89d820366ca9b71431ff29615c14e8e10
vish-6 <vishnuvardhan.j@thinkpalm.com>
2025-02-12 12:01:31 +0530

    fixed some issues with prometheus deployment. added kubernetes-nodes job in prometheus.yml

--- wordpress-with-prometheus/prometheus/prerequisites.sh
+++ wordpress-with-prometheus/prometheus/prerequisites.sh
@@ -3,4 +3,5 @@ kubectl create configmap --from-file prometheus.yml prometheus-configuration
 kubectl create configmap --from-file alertmanager.yml alert-config
 kubectl create configmap --from-file rules.yaml rules-config
 kubectl create sa prometheus
-kubectl create token prometheus
+TOKEN=$(kubectl create token prometheus)
+sed -i "s|kubernetes-nodes-bearer-token|$TOKEN|g" prometheus.yml
